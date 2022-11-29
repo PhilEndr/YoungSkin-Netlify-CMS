a64a6b11df1c56c6660bbc54ba6cd01d9fe42959
PhilEnd <philipp.endrulat@gmail.com>
2022-11-29 01:04:47 +0100

    Updated Stuff

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -79,12 +79,12 @@ const Navbar = class extends React.Component {
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://www.google.com"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <span className="icon">
-                  <img src={github} alt="Github" />
+                  todo: Cart Image
                 </span>
               </a>
             </div>
